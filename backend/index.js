5bc27ee04c0038be8eb3ad000ca20d6f4ba024a0
mostafa <mostafameerzad@gmail.com>
2024-05-03 19:34:10 +0430

    Login route Complete creating login route for the users

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,29 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 const app = express();
 const PORT = 3000;
 const users = [];
 
 app.use(express.json());
 
+//configure passport's localStrategy
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    const user = users.find((user) => user.username === username);
+    if (!user) return done(null, false);
+    if (!bcrypt.compare(password, user.password)) return done(null, false);
+    return done(null, user);
+  })
+);
+
 app.post("/signup", async (req, res) => {
   // parse req.body to get needed fields
   const { username, password } = req.body;
+  console.log(username)
+  console.log(password)
   // check if user already exists
   const existingUser = users.find((user) => user.username === username);
 
@@ -34,11 +47,28 @@ app.post("/signup", async (req, res) => {
       expiresIn: "1h",
     });
     // send token in a cookie
-    res.cookie(token, token, { httpOnly: true, secure: true });
+    // res.cookie("token", token, { httpOnly: true, secure: true });
+    console.log("token created")
     res
       .status(201)
       .json({ message: "User created", userId: newUser.id, token });
   } catch (err) {
     res.status(500).json({ message: "Error registering new user" });
   }
+});
+
+app.post(
+  "/login",
+  passport.authenticate("local", { session: false }),
+  (req, res) => {
+    const token = jwt.sign(req.body.userId, "my-app-secret", {
+      expiresIn: "1h",
+    });
+    res.cookie("token", token, { httpOnly: true });
+    res.json({ success: true, token });
+  }
+);
+
+app.listen(PORT, () => {
+  console.log("server listening on port: ", PORT);
 });
